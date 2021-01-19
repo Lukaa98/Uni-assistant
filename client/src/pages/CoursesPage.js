e238899db680c7f094ae813a1756259f800215d6
Lukaa98 <42852367+Lukaa98@users.noreply.github.com>
2021-01-18 23:19:40 -0500

    Create CoursesPage.js

---
+++ client/src/pages/CoursesPage.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Course from '../components/Course';
+import '../css/allcourses.css';
+
+class CoursesPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.fetchCourses = this.fetchCourses.bind(this);
+    }
+
+    state = {
+        courseData: []
+    }
+
+    fetchCourses() {
+        fetch('api/courses')
+        .then(res => res.json())
+        .then(results => {
+            this.setState({
+                courseData: results
+            })
+
+            console.log(results);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    componentDidMount() {
+        this.fetchCourses();
+    }
+
+    render() {
+        return(
+            <div className="container-fluid text-center">
+                <div className="row justify-content-center">
+                    <div>
+                        <h1 className="text-primary">Courses</h1>
+                        <div className="courseBtn">
+                            <div className="row">
+                                <div className="col">
+                                    <Link to="/new-course" className="btn btn-primary">Add Course</Link>
+                                </div>
+                                <div className="col">
+                                    <Link to="/new-school-department" className="schooldepartment btn btn-primary">Add Department / School</Link>
+                                </div>
+                            </div>
+                            
+                            
+                        </div>
+
+                        {
+                            this.state.courseData.map(course => (
+                                <Course 
+                                key={course.id}
+                                id={course.id}
+                                courseName={course.name}
+                                schoolid={course.schoolId}
+                                departmentid={course.departmentId}
+                                courseCode={course.course_code}
+                                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis egestas ipsum facilisis convallis. Curabitur placerat ex at metus ultricies, nec dictum leo pharetra. Ut auctor velit in laoreet ultrices."
+                                />
+                            ))
+                        }
+
+                        
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    
+}
+
+export default CoursesPage;
